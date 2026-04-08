50c334e7cf3c50261450f7580734d2fa8f61a965
Craw-Dope <trev.hill@gmail.com>
2026-04-08 19:34:12 -0400

    Basic droplet addition
    Added droplet scene. Its attached script has it fall when it spawns in. The logic for it spawning in is in the _ready() and _on_timer_timeout() functions.

---
+++ Scenes/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var timer = Timer.new()
+	timer.wait_time = 1.7
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+	
+func _on_timer_timeout():
+	randomize()
+	var droplet = preload("res://Scenes/falling_droplet.tscn").instantiate()
+	droplet.position.y = -25
+	droplet.position.x = randf_range(100, 1000)
+	add_child(droplet)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
