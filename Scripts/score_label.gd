396b7a3ff240abee2a0d30fafd17a8d4b72d7693
Craw-Dope <trev.hill@gmail.com>
2026-04-09 15:07:14 -0400

    Varied Drop Rates
    Added a Timer node to Main node to handle random timing for drop rate.

--- Scripts/score_label.gd
+++ Scripts/score_label.gd
@@ -4,6 +4,6 @@ func _ready():
 	GameManager.score_changed.connect(_on_score_changed)
 	
 func _on_score_changed(new_score):
-	text = "Score: " + str(new_score)
+	text = "Score: " + str(int(new_score))
 	
 	
